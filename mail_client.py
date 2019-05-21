afd3d6a2ff7afe58d77a5dd5742a0c3385d93d60
salem8171 <39016808+salem8171@users.noreply.github.com>
2019-05-21 22:32:53 +0100

    Update mail_client.py

--- mail_client.py
+++ mail_client.py
@@ -4,7 +4,7 @@ def sendmail(msg):
         # mail_server.connect('smtp.gmail.com', 465)
         mail_server.ehlo()
         mail_server.starttls()
-        mail_server.login("s28260962@gmail.com", "M$K28260962")
+        mail_server.login("s28260962@gmail.com", "password")
         mail_server.sendmail("s28260962@gmail.com", "yosr.benhamida@enis.tn", msg)
         mail_server.close()
     except:
